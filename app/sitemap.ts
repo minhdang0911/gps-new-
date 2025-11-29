8b48aa8b734272219acb14908ca1782a434ad5c1
minhdang0911 <121071062+minhdang0911@users.noreply.github.com>
2025-11-29 16:59:57 +0700

    update sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,43 @@
+// app/sitemap.ts
+export default function sitemap() {
+    const baseUrl = 'https://gps-bms-tracking.iky.vn';
+
+    return [
+        {
+            url: `${baseUrl}/`,
+            lastModified: new Date(),
+            changefreq: 'always',
+            priority: 1.0,
+        },
+        {
+            url: `${baseUrl}/cruise`,
+            lastModified: new Date(),
+            changefreq: 'daily',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/report`,
+            lastModified: new Date(),
+            changefreq: 'daily',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/report/usage-session`,
+            lastModified: new Date(),
+            changefreq: 'daily',
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/manage/devices`,
+            lastModified: new Date(),
+            changefreq: 'daily',
+            priority: 0.7,
+        },
+        {
+            url: `${baseUrl}/support`,
+            lastModified: new Date(),
+            changefreq: 'monthly',
+            priority: 0.5,
+        },
+    ];
+}
